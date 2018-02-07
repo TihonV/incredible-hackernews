8461cc060e5f40b08db6520f7791cbda0ae5b700
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2018-02-08 00:19:12 +0200

    Redirect comoponent

---
+++ src/components/Redirect/index.js
@@ -0,0 +1,18 @@
+import { Component } from 'preact';
+import { route } from 'preact-router';
+
+import PropTypes from 'prop-types';
+
+export default class Redirect extends Component {
+  componentWillMount() {
+    route(this.props.to, true);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+Redirect.propTypes = {
+  to: PropTypes.string.isRequired,
+};
